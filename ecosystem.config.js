81e8f2c5f24bb350e06b1b12f34a82a26ec38be8
Sefinek <contact@sefinek.net>
2024-12-19 15:55:58 +0100

    Update README.md etc

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,1 +1,26 @@
-module.exports = {};
+module.exports = {
+	apps: [{
+		name: 'ufw-abuseipdb',
+		script: './index.js',
+
+		// Logging configuration
+		log_date_format: 'HH:mm:ss.SSS DD.MM.YYYY',
+		merge_logs: true,
+		log_file: '/var/log/ufw-abuseipdb/combined.log',
+		out_file: '/var/log/ufw-abuseipdb/out.log',
+		error_file: '/var/log/ufw-abuseipdb/error.log',
+
+		// Application restart policy settings
+		wait_ready: true,
+		autorestart: true,
+		max_restarts: 10,
+		min_uptime: 20000,
+		restart_delay: 5000,
+		exp_backoff_restart_delay: 3000,
+
+		// Environment variables configuration
+		env: {
+			NODE_ENV: 'production',
+		},
+	}],
+};
